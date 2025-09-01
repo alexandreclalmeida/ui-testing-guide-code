2b5d4bdd3b995d9f52a1b493c28492850e345d30
Alexandre Lopes de Almeida <alexandreclalmeida@gmail.com>
2025-09-01 11:21:10 -0300

    Accessibility chapter

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@
 const config = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
-  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
